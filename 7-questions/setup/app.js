fea5c459d10a39473e3168eae01a3a599516006e
Jairo Espinel <jairo25509@hotmail.com>
2021-04-05 09:22:17 -0500

    Questions updated w/ my version but missing the functionality to hide the open answer when a new one is clicked

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,23 @@
 //using selectors inside the element
+// const preguntas = document.querySelectorAll(".question");
+
+// preguntas.forEach(function(elem){
+//     const boton = elem.querySelector(".question-btn")
+//     boton.addEventListener("click", function(){
+//         elem.classList.toggle("show-text")
+//     })
+//     if (elem!==)
+//     console.log(boton)
+// })
+
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (pepapig) {
+    const question = pepapig.currentTarget.parentElement.parentElement;
+
+    question.classList.toggle("show-text");
+  });
+});
