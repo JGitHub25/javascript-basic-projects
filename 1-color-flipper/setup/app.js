732dbfb5a696256a30b22c6c0629265df010752d
Jairo Espinel <jairo25509@hotmail.com>
2021-04-05 08:51:58 -0500

    Color-flipper updated and working

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const boton = document.getElementById("btn");
+const nombreColor = document.querySelector(".color");
+
+boton.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 4);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  nombreColor.innerHTML = colors[randomNumber];
+});
--- 1-color-flipper/setup/hex.js
+++ 1-color-flipper/setup/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const boton = document.getElementById("btn");
+const nombreColor = document.querySelector(".color");
+
+function colorHex() {
+  let colorRandom = ["#"]; // En vez de hacer esta variable un array, él la hizo una string. Mejor: se ahorraba el .join("").
+
+  for (let i = 0; i < 6; i++) {
+    colorRandom.push(hex[Math.floor(Math.random() * hex.length)]);
+  }
+  let stringColor = colorRandom.join("");
+
+  document.body.style.backgroundColor = stringColor;
+  nombreColor.innerHTML = stringColor;
+}
+
+boton.addEventListener("click", colorHex);
