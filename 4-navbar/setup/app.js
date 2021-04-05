f3e6659ad020b5b4374559e8b04874e1e517e944
Jairo Espinel <jairo25509@hotmail.com>
2021-04-05 09:09:52 -0500

    Navbar updated w/ my version and working

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+function esconderMostrar() {
+  document.querySelector(".links").classList.toggle("show-links");
+}
+
+document
+  .querySelector(".nav-toggle")
+  .addEventListener("click", esconderMostrar);
