e1adc14cc317bdfb797602ff9b9a2dc3378a41b0
Jairo Espinel <jairo25509@hotmail.com>
2021-04-08 08:40:11 -0500

    Video project working. Scroll w/ just some initial HTML

--- 9-video/setup/app.js
+++ 9-video/setup/app.js
@@ -1,3 +1,24 @@
 // MDN
-// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
+// The DOMContentLoaded event fires when the initial HTML document
+// has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
+
+// Botón para play/pause el video
+let video = document.querySelector(".video-container");
+
+let botonSwitch = document.querySelector(".switch-btn");
+
+botonSwitch.addEventListener("click", function () {
+  botonSwitch.classList.toggle("slide");
+
+  if (botonSwitch.classList.contains("slide")) {
+    video.pause();
+  } else {
+    video.play();
+  }
+});
+
+// Ocultar gif preloader una vez todo ha cargado.
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("hide-preloader");
+});
